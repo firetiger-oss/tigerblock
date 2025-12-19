daa3d4f934858d0c927af5f7c1e27339c0c4ab67
Jacob Marble <jacobmarble@gmail.com>
2025-12-18 18:53:35 -0800

    fix: secret/env init() registration prefix

--- secret/env/registry.go
+++ secret/env/registry.go
@@ -19,6 +19,9 @@ func (registry) LoadManager(ctx context.Context, identifier string) (secret.Mana
 
 func (registry) ParseSecret(identifier string) (managerID, secretName, version string, err error) {
 	// Format: env:ENV_VAR_NAME
-	managerID, secretName, _ = strings.Cut(identifier, ":")
+	if strings.HasPrefix(identifier, "env:") {
+		managerID = "env:"
+		secretName = strings.TrimPrefix(identifier, "env:")
+	}
 	return
 }
